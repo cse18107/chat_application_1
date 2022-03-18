b9acc6daf4e759f8f67d9ff6fe21701e238a2b35
cse18107 <cse18107@cemk.ac.in>
2022-03-18 13:48:49 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const express = require('express');
+const cors =require('cors');
+const socketIO = require('socket.io');
+
+const app = express();
+const port = 4500 || process.env.PORT;
+
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.send("It is working");
+})
+
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+io.on('connection',()=>{
+    console.log('New connection');
+});
+
+server.listen(port,()=>{
+    console.log(`Server is working on ${port}`);
+})
+
